031c16d4eb810de86a2e54e5f9892e717d62f44d
Thomas McGoey-Smith <thomas@tamcgoey.com>
2017-01-21 22:39:43 -0500

    fixed docker compose to actually work

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://192.168.99.100:6379/12' }
+  config.redis = { url: 'redis://localhost:6379/12' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://192.168.99.100:6379/12' }
+  config.redis = { url: 'redis://localhost:6379/12' }
 end
